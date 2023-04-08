5ee1bb4262dde499e5ff4e6206c7d3b69e25ae51
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-08 15:09:37 +0300

    ex_3 task done

--- Python-Lists/Python_Lists.py
+++ Python-Lists/Python_Lists.py
@@ -1,1 +1,16 @@
+import random
 
+try:
+    my_list = []
+    sum = 0
+    for i in range(0, 14):
+        my_list.append(random.randint(0, 12))
+        if my_list[i] % 2 != 0:
+            print("Odd number", my_list[i])
+            sum += my_list[i]
+        else:
+            print("Even number", my_list[i])
+    print("List", my_list)
+    print("Sum", sum)
+except:
+    print("Error: List is not defined")
