7f837782a89242763cd03d2fee65c6abdad0005f
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-08 15:42:32 +0300

    Task done

--- Python-Lists/Python_Lists.py
+++ Python-Lists/Python_Lists.py
@@ -1,1 +1,23 @@
+import random
+
+
+my_list = [random.randint(-10, 10) for i in range(20)]
+
+
+first_positive_index = 0
+while my_list[first_positive_index] <= 0:
+    first_positive_index += 1
+
+
+last_positive_index = len(my_list) - 1
+while my_list[last_positive_index] <= 0:
+    last_positive_index -= 1
+
+
+sum_between = 0
+for i in range(first_positive_index + 1, last_positive_index):
+    sum_between += my_list[i]
+
+print("List:", my_list)
+print("Sum of elements between the first and last positive element:", sum_between)
 
