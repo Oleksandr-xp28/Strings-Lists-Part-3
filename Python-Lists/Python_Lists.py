d12ba9530e4f1f29ca57d7fc00e709a396b0e6cf
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-08 16:03:21 +0300

    ex_6 task done

--- Python-Lists/Python_Lists.py
+++ Python-Lists/Python_Lists.py
@@ -1,1 +1,12 @@
+import random
 
+try:
+    # Create an empty list
+    my_list = [] 
+    for i in range(20):
+        my_list.append(random.randint(0, 15))
+        
+        if my_list[i] % 2 == 0:
+            print("Even:", my_list[i])
+except Exception as e:
+    print("Error:", e)
